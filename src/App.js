1528f34713a8c6640a25fa4f42ad0d3bc99bc30d
zm00622 <zarymanning@gmail.com>
2021-02-28 15:20:28 -0500

    This commit contains a bootsrap navbar and what will be an image grid that utilizes flexbox.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,47 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
+     {/* This is navbar html from bootstrap */}
+
+     <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+        {/* End of navbar html from bootstrap */}
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +59,16 @@ function App() {
           Learn React
         </a>
       </header>
+
+      {/* Responsive Image Blocks Using Flexbox */}
+
+      <div class="flex-container">
+      <div class="flex-item-left">1</div>
+      <div class="flex-item-right">2</div>
+      </div>
+
+      {/* End of Responsive Image Blocks Using Flexbox */}
+
     </div>
   );
 }
